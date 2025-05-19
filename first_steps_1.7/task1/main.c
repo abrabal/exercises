37a6c82cd472cdece6c972cb407943479ff7994d
artem <artemzaiets21@gmail.com>
2025-05-19 18:51:26 +0000

    chapter 1.7 completed

--- first_steps_1.7/task1/main.c
+++ first_steps_1.7/task1/main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 
+float fahrtocels (float fahrenheit);
+
 int main()
 {
+    int upper, step;
     
+    upper = 300;
+    step = 20;
+
+    for (float fahr = 0; fahr <= upper; fahr = fahr + step){
+        printf("F: %.0f\tC: %.1f\n", fahr, fahrtocels(fahr));
+    }
 
     return 0;
+}
+
+float fahrtocels (float fahr)
+{
+    float c;
+
+    c = (fahr - 32.0)/1.8;
+
+    return c;
 }
